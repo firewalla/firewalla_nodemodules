8b777790af1c15e6ffa5ed1d7d980e8156401878
Melvin Tu <melvinto@gmail.com>
2016-12-21 12:54:13 +0000

    init batch 2

---
+++ .0.1.4@xpc-connection/build/Release/.deps/Release/obj.target/binding.node.d
@@ -0,0 +1,1 @@
+cmd_Release/obj.target/binding.node := g++ -shared -pthread -rdynamic  -Wl,-soname=binding.node -o Release/obj.target/binding.node -Wl,--start-group  -Wl,--end-group 
---
+++ .1.2.0@usb/build/Release/.deps/Release/obj.target/usb_bindings/src/transfer.o.d
@@ -0,0 +1,62 @@
+cmd_Release/obj.target/usb_bindings/src/transfer.o := g++ '-DNODE_GYP_MODULE_NAME=usb_bindings' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I../src -I../../.2.4.0@nan -I/home/pi/.node-gyp/4.6.1/include/node -I/home/pi/.node-gyp/4.6.1/src -I/home/pi/.node-gyp/4.6.1/deps/uv/include -I/home/pi/.node-gyp/4.6.1/deps/v8/include -I../libusb/libusb  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -std=c++0x -MMD -MF ./Release/.deps/Release/obj.target/usb_bindings/src/transfer.o.d.raw   -c -o Release/obj.target/usb_bindings/src/transfer.o ../src/transfer.cc
+Release/obj.target/usb_bindings/src/transfer.o: ../src/transfer.cc \
+ ../src/node_usb.h ../libusb/libusb/libusb.h \
+ /home/pi/.node-gyp/4.6.1/include/node/v8.h \
+ /home/pi/.node-gyp/4.6.1/include/node/v8-version.h \
+ /home/pi/.node-gyp/4.6.1/include/node/v8config.h \
+ /home/pi/.node-gyp/4.6.1/include/node/node.h \
+ /home/pi/.node-gyp/4.6.1/include/node/v8.h \
+ /home/pi/.node-gyp/4.6.1/include/node/node_version.h \
+ /home/pi/.node-gyp/4.6.1/include/node/node_buffer.h \
+ /home/pi/.node-gyp/4.6.1/include/node/node.h \
+ /home/pi/.node-gyp/4.6.1/include/node/uv.h \
+ /home/pi/.node-gyp/4.6.1/include/node/uv-errno.h \
+ /home/pi/.node-gyp/4.6.1/include/node/uv-version.h \
+ /home/pi/.node-gyp/4.6.1/include/node/uv-unix.h \
+ /home/pi/.node-gyp/4.6.1/include/node/uv-threadpool.h \
+ /home/pi/.node-gyp/4.6.1/include/node/uv-linux.h ../src/helpers.h \
+ ../../.2.4.0@nan/nan.h \
+ /home/pi/.node-gyp/4.6.1/include/node/node_version.h \
+ /home/pi/.node-gyp/4.6.1/include/node/node_object_wrap.h \
+ ../../.2.4.0@nan/nan_callbacks.h ../../.2.4.0@nan/nan_callbacks_12_inl.h \
+ ../../.2.4.0@nan/nan_maybe_43_inl.h ../../.2.4.0@nan/nan_converters.h \
+ ../../.2.4.0@nan/nan_converters_43_inl.h ../../.2.4.0@nan/nan_new.h \
+ ../../.2.4.0@nan/nan_implementation_12_inl.h \
+ ../../.2.4.0@nan/nan_persistent_12_inl.h ../../.2.4.0@nan/nan_weak.h \
+ ../../.2.4.0@nan/nan_object_wrap.h \
+ ../../.2.4.0@nan/nan_typedarray_contents.h ../src/polyfill.h \
+ ../src/uv_async_queue.h
+../src/transfer.cc:
+../src/node_usb.h:
+../libusb/libusb/libusb.h:
+/home/pi/.node-gyp/4.6.1/include/node/v8.h:
+/home/pi/.node-gyp/4.6.1/include/node/v8-version.h:
+/home/pi/.node-gyp/4.6.1/include/node/v8config.h:
+/home/pi/.node-gyp/4.6.1/include/node/node.h:
+/home/pi/.node-gyp/4.6.1/include/node/v8.h:
+/home/pi/.node-gyp/4.6.1/include/node/node_version.h:
+/home/pi/.node-gyp/4.6.1/include/node/node_buffer.h:
+/home/pi/.node-gyp/4.6.1/include/node/node.h:
+/home/pi/.node-gyp/4.6.1/include/node/uv.h:
+/home/pi/.node-gyp/4.6.1/include/node/uv-errno.h:
+/home/pi/.node-gyp/4.6.1/include/node/uv-version.h:
+/home/pi/.node-gyp/4.6.1/include/node/uv-unix.h:
+/home/pi/.node-gyp/4.6.1/include/node/uv-threadpool.h:
+/home/pi/.node-gyp/4.6.1/include/node/uv-linux.h:
+../src/helpers.h:
+../../.2.4.0@nan/nan.h:
+/home/pi/.node-gyp/4.6.1/include/node/node_version.h:
+/home/pi/.node-gyp/4.6.1/include/node/node_object_wrap.h:
+../../.2.4.0@nan/nan_callbacks.h:
+../../.2.4.0@nan/nan_callbacks_12_inl.h:
+../../.2.4.0@nan/nan_maybe_43_inl.h:
+../../.2.4.0@nan/nan_converters.h:
+../../.2.4.0@nan/nan_converters_43_inl.h:
+../../.2.4.0@nan/nan_new.h:
+../../.2.4.0@nan/nan_implementation_12_inl.h:
+../../.2.4.0@nan/nan_persistent_12_inl.h:
+../../.2.4.0@nan/nan_weak.h:
+../../.2.4.0@nan/nan_object_wrap.h:
+../../.2.4.0@nan/nan_typedarray_contents.h:
+../src/polyfill.h:
+../src/uv_async_queue.h:

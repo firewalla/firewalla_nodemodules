b464974962599271118d2386632962929c1f3afd
Melvin Tu <melvinto@gmail.com>
2016-12-23 13:39:32 +0000

    add modules

---
+++ .0.3.1@pty.js/build/Release/.deps/Release/obj.target/pty/src/unix/pty.o.d
@@ -0,0 +1,53 @@
+cmd_Release/obj.target/pty/src/unix/pty.o := g++ '-DNODE_GYP_MODULE_NAME=pty' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/4.6.2/include/node -I/home/pi/.node-gyp/4.6.2/src -I/home/pi/.node-gyp/4.6.2/deps/uv/include -I/home/pi/.node-gyp/4.6.2/deps/v8/include -I../../.2.3.5@nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/pty/src/unix/pty.o.d.raw   -c -o Release/obj.target/pty/src/unix/pty.o ../src/unix/pty.cc
+Release/obj.target/pty/src/unix/pty.o: ../src/unix/pty.cc \
+ ../../.2.3.5@nan/nan.h \
+ /home/pi/.node-gyp/4.6.2/include/node/node_version.h \
+ /home/pi/.node-gyp/4.6.2/include/node/uv.h \
+ /home/pi/.node-gyp/4.6.2/include/node/uv-errno.h \
+ /home/pi/.node-gyp/4.6.2/include/node/uv-version.h \
+ /home/pi/.node-gyp/4.6.2/include/node/uv-unix.h \
+ /home/pi/.node-gyp/4.6.2/include/node/uv-threadpool.h \
+ /home/pi/.node-gyp/4.6.2/include/node/uv-linux.h \
+ /home/pi/.node-gyp/4.6.2/include/node/node.h \
+ /home/pi/.node-gyp/4.6.2/include/node/v8.h \
+ /home/pi/.node-gyp/4.6.2/include/node/v8-version.h \
+ /home/pi/.node-gyp/4.6.2/include/node/v8config.h \
+ /home/pi/.node-gyp/4.6.2/include/node/node_version.h \
+ /home/pi/.node-gyp/4.6.2/include/node/node_buffer.h \
+ /home/pi/.node-gyp/4.6.2/include/node/node.h \
+ /home/pi/.node-gyp/4.6.2/include/node/node_object_wrap.h \
+ ../../.2.3.5@nan/nan_callbacks.h ../../.2.3.5@nan/nan_callbacks_12_inl.h \
+ ../../.2.3.5@nan/nan_maybe_43_inl.h ../../.2.3.5@nan/nan_converters.h \
+ ../../.2.3.5@nan/nan_converters_43_inl.h ../../.2.3.5@nan/nan_new.h \
+ ../../.2.3.5@nan/nan_implementation_12_inl.h \
+ ../../.2.3.5@nan/nan_persistent_12_inl.h ../../.2.3.5@nan/nan_weak.h \
+ ../../.2.3.5@nan/nan_object_wrap.h \
+ ../../.2.3.5@nan/nan_typedarray_contents.h
+../src/unix/pty.cc:
+../../.2.3.5@nan/nan.h:
+/home/pi/.node-gyp/4.6.2/include/node/node_version.h:
+/home/pi/.node-gyp/4.6.2/include/node/uv.h:
+/home/pi/.node-gyp/4.6.2/include/node/uv-errno.h:
+/home/pi/.node-gyp/4.6.2/include/node/uv-version.h:
+/home/pi/.node-gyp/4.6.2/include/node/uv-unix.h:
+/home/pi/.node-gyp/4.6.2/include/node/uv-threadpool.h:
+/home/pi/.node-gyp/4.6.2/include/node/uv-linux.h:
+/home/pi/.node-gyp/4.6.2/include/node/node.h:
+/home/pi/.node-gyp/4.6.2/include/node/v8.h:
+/home/pi/.node-gyp/4.6.2/include/node/v8-version.h:
+/home/pi/.node-gyp/4.6.2/include/node/v8config.h:
+/home/pi/.node-gyp/4.6.2/include/node/node_version.h:
+/home/pi/.node-gyp/4.6.2/include/node/node_buffer.h:
+/home/pi/.node-gyp/4.6.2/include/node/node.h:
+/home/pi/.node-gyp/4.6.2/include/node/node_object_wrap.h:
+../../.2.3.5@nan/nan_callbacks.h:
+../../.2.3.5@nan/nan_callbacks_12_inl.h:
+../../.2.3.5@nan/nan_maybe_43_inl.h:
+../../.2.3.5@nan/nan_converters.h:
+../../.2.3.5@nan/nan_converters_43_inl.h:
+../../.2.3.5@nan/nan_new.h:
+../../.2.3.5@nan/nan_implementation_12_inl.h:
+../../.2.3.5@nan/nan_persistent_12_inl.h:
+../../.2.3.5@nan/nan_weak.h:
+../../.2.3.5@nan/nan_object_wrap.h:
+../../.2.3.5@nan/nan_typedarray_contents.h:

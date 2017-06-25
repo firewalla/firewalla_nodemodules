f015fbc75774d366d6b5b2d1e23036015a62c4c2
Melvin Tu <melvinto@gmail.com>
2017-06-25 09:36:26 +0000

    add heapdump

---
+++ heapdump/build/Release/.deps/Release/obj.target/addon/src/heapdump.o.d
@@ -0,0 +1,34 @@
+cmd_Release/obj.target/addon/src/heapdump.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/4.8.1/include/node -I/home/pi/.node-gyp/4.8.1/src -I/home/pi/.node-gyp/4.8.1/deps/uv/include -I/home/pi/.node-gyp/4.8.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/addon/src/heapdump.o.d.raw   -c -o Release/obj.target/addon/src/heapdump.o ../src/heapdump.cc
+Release/obj.target/addon/src/heapdump.o: ../src/heapdump.cc \
+ /home/pi/.node-gyp/4.8.1/include/node/node.h \
+ /home/pi/.node-gyp/4.8.1/include/node/v8.h \
+ /home/pi/.node-gyp/4.8.1/include/node/v8-version.h \
+ /home/pi/.node-gyp/4.8.1/include/node/v8config.h \
+ /home/pi/.node-gyp/4.8.1/include/node/node_version.h ../src/compat-inl.h \
+ ../src/compat.h /home/pi/.node-gyp/4.8.1/include/node/node_version.h \
+ /home/pi/.node-gyp/4.8.1/include/node/v8.h \
+ /home/pi/.node-gyp/4.8.1/include/node/v8-profiler.h \
+ /home/pi/.node-gyp/4.8.1/include/node/uv.h \
+ /home/pi/.node-gyp/4.8.1/include/node/uv-errno.h \
+ /home/pi/.node-gyp/4.8.1/include/node/uv-version.h \
+ /home/pi/.node-gyp/4.8.1/include/node/uv-unix.h \
+ /home/pi/.node-gyp/4.8.1/include/node/uv-threadpool.h \
+ /home/pi/.node-gyp/4.8.1/include/node/uv-linux.h ../src/heapdump-posix.h
+../src/heapdump.cc:
+/home/pi/.node-gyp/4.8.1/include/node/node.h:
+/home/pi/.node-gyp/4.8.1/include/node/v8.h:
+/home/pi/.node-gyp/4.8.1/include/node/v8-version.h:
+/home/pi/.node-gyp/4.8.1/include/node/v8config.h:
+/home/pi/.node-gyp/4.8.1/include/node/node_version.h:
+../src/compat-inl.h:
+../src/compat.h:
+/home/pi/.node-gyp/4.8.1/include/node/node_version.h:
+/home/pi/.node-gyp/4.8.1/include/node/v8.h:
+/home/pi/.node-gyp/4.8.1/include/node/v8-profiler.h:
+/home/pi/.node-gyp/4.8.1/include/node/uv.h:
+/home/pi/.node-gyp/4.8.1/include/node/uv-errno.h:
+/home/pi/.node-gyp/4.8.1/include/node/uv-version.h:
+/home/pi/.node-gyp/4.8.1/include/node/uv-unix.h:
+/home/pi/.node-gyp/4.8.1/include/node/uv-threadpool.h:
+/home/pi/.node-gyp/4.8.1/include/node/uv-linux.h:
+../src/heapdump-posix.h:
